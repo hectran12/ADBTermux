fcdaece81e5936e670509fa364d5af93509121f1
Héc Trần <86024288+hectran12@users.noreply.github.com>
2023-06-11 11:31:45 +0700

    Update InstallTools.sh
    Việt hoá và change repo install

--- TermuxADBFiles/InstallTools.sh
+++ TermuxADBFiles/InstallTools.sh
@@ -1,32 +1,32 @@
 #!/data/data/com.termux/files/usr/bin/bash
-
+# việt hoá by Hectran =))
 directory="$(pwd)"
 echo
-echo -e "\e[93mThis script will install ADB & FastBoot Tools in Termux."
+echo -e "\e[93mKịch bản này tải công cụ ADB & FastBoot trên termux"
 echo
-echo -e "\e[32m[*] \e[34mDownloading wrapper script..."
+echo -e "\e[32m[*] \e[34mKhu vực này đang tải tệp lệnh"
 mkdir $PREFIX/tmp/adbtemp
 downpath="$PREFIX/tmp/adbtemp"
-wget https://github.com/MasterDevX/Termux-ADB/raw/master/bin/adb -P $downpath/ -q
-echo -e "\e[32m[*] \e[34mDownloading binaries..."
-wget https://github.com/MasterDevX/Termux-ADB/raw/master/bin/adb.bin -P $downpath/ -q
-wget https://github.com/MasterDevX/Termux-ADB/raw/master/bin/fastboot -P $downpath/ -q
-wget https://github.com/MasterDevX/Termux-ADB/raw/master/bin/fastboot-armeabi -P $downpath/ -q
-echo -e "\e[32m[*] \e[34mCopying files to bin..."
+wget https://raw.githubusercontent.com/hectran12/ADBTermux/main/TermuxADBFiles/bin/adb -P $downpath/ -q
+echo -e "\e[32m[*] \e[34mĐang tải xuống các tệp nhị phân..."
+wget https://github.com/hectran12/ADBTermux/raw/main/TermuxADBFiles/bin/adb.bin -P $downpath/ -q
+wget https://github.com/hectran12/ADBTermux/raw/main/TermuxADBFiles/bin/fastboot -P $downpath/ -q
+wget https://github.com/hectran12/ADBTermux/raw/main/TermuxADBFiles/bin/fastboot-armeabi -P $downpath/ -q
+echo -e "\e[32m[*] \e[34mĐang sao chép tệp vào thư mục bin..."
 cp $downpath/* $PREFIX/bin
-echo -e "\e[32m[*] \e[34mSetting execution permissions..."
+echo -e "\e[32m[*] \e[34mĐang thiết lập quyền thực thi..."
 files="$(ls $downpath)"
 cd $PREFIX/bin
 chmod +x $files
-echo -e "\e[32m[*] \e[34mCreating workspace directory..."
+echo -e "\e[32m[*] \e[34mĐang tạo khu làm việc..."
 cd $HOME
 if [ ! -d "adbfiles" ]; then
   mkdir adbfiles
 fi
-echo -e "\e[32m[*] \e[34mCleaning up..."
+echo -e "\e[32m[*] \e[34mĐang xoá file cài đặt..."
 cd $directory
 rm -rf $downpath
 rm -rf InstallTools.sh
 echo
-echo -e "\e[32mTools were successfully installed!\e[39m"
+echo -e "\e[32mCông cụ đã được cài hoàn tất!\e[39m"
 echo
